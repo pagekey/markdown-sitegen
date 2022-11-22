0e4cd939b664afabbdc4ab57f1af17097a2e819b
Stephen Grice <stephengrice@live.com>
2022-11-21 23:31:57 -0500

    Print abspath to first arg

--- app/index.js
+++ app/index.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
-console.log('hello world')
+const path = require('path');
+
+function print_usage() {
+    console.error('Usage: sitegen <content_dir>');
+}
+
+function main() {
+    // note: eventually can use 'yargs' pkg to add more functionality / CLI options
+    let args = process.argv.slice(2);
+    
+    if (args.length != 1) {
+        print_usage();
+        return;
+    }
+
+    let content_dir = path.resolve(args[0]);
+    console.log(content_dir);
+}
+main();
