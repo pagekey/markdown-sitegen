61aa938bdccffb278fb3dc6d6d3feb960ee0f2a5
Stephen Grice <stephengrice@live.com>
2023-01-28 18:53:15 -0500

    Iterate markdown files

--- markdown_sitegen/cli.py
+++ markdown_sitegen/cli.py
@@ -1,3 +1,24 @@
+import glob
+import os
+import sys
+
+
+def print_usage():
+    print("Usage:")
+    print("  markdown-sitegen <DIRECTORY>: generate website from markdown in directory")
+
 
 def cli_entry_point():
-    print("Hello world")
+    if len(sys.argv) == 2:
+        gen_dir = sys.argv[1]
+        if os.path.exists(gen_dir) and os.path.isdir(gen_dir):
+            files_to_render = []
+            for root, dirs, files in os.walk(gen_dir):
+                for filename in files:
+                    if filename.lower().endswith('.md'):
+                        files_to_render.append(filename)
+            print(files_to_render, len(files_to_render))
+        else:
+            print("Error: not a directory - %s" % gen_dir)
+    else:
+        print_usage()
