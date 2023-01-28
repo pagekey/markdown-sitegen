ceba7155af80b046a5c25acdbba627115c880856
Stephen Grice <stephengrice@live.com>
2023-01-28 18:44:19 -0500

    Add basic CLI entry point

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+
+setup(
+    name='markdown-sitegen',
+    version='1.0.0',
+    description='Markdown site generator',
+    author='Steve G',
+    author_email='steve@pagekeytech.com',
+    packages=['markdown_sitegen'],
+    entry_points={
+        'console_scripts': ['markdown-sitegen=markdown_sitegen.cli:cli_entry_point'],
+    },
+    install_requires=[
+        'numpy',
+        'pyyaml',
+        'PyQt5'
+    ],
+)
